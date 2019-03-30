a28aa7e896600a27df19b70bd0a6027a28b9c764
Noa Kolodro <noakolodro@tzahi-shemeshs-iMac.local>
2019-03-31 00:03:50 +0300

    temporarly adding build folder

---
+++ build/precache-manifest.fb410c86013f8a97bad53fab01533726.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "05f1e922fce49f2e1e84e81fc06bf4b3",
+    "url": "/static/media/share-icon.05f1e922.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0c514ad7dc4f814fcf56",
+    "url": "/static/js/main.ba7447c8.chunk.js"
+  },
+  {
+    "revision": "3a67bb73ee1c59097045",
+    "url": "/static/js/2.769b11c0.chunk.js"
+  },
+  {
+    "revision": "0c514ad7dc4f814fcf56",
+    "url": "/static/css/main.33687dd8.chunk.css"
+  },
+  {
+    "revision": "abc7e4d3b46481a51fecd657f9d8ee20",
+    "url": "/index.html"
+  }
+];
